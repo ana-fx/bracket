2867f372a09ca100a86184fba87d777dea81f998
Ana <aanjr38@gmail.com>
2025-12-21 16:23:28 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="antialiased">
+    <div class="flex items-center justify-center min-h-screen">
+        <h1 class="text-3xl font-bold underline">
+            Clean Home
+        </h1>
+    </div>
+</body>
+</html>
